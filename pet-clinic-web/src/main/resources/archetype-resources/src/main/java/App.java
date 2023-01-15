24bc0a84769f6a5656a865618fc99a7f000c99aa
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-01-15 17:36:53 +0100

    create two modules

---
+++ pet-clinic-web/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.sv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
