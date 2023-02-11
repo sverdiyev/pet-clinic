ec3625d8889c3631524c5b3fac44ec93a5a530f8
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-02-11 13:21:11 +0100

    add lombock to data module and create BaseEntity as JPA Superclass Entity

--- pet-clinic-data/src/main/java/com/sv/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/sv/petclinic/model/BaseEntity.java
@@ -1,15 +1,20 @@
 package com.sv.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
 //tutor implemented Serializable, I do not know if it is required at the moment, so omit it
+@MappedSuperclass
+@Getter
+@Setter
 public class BaseEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
 }
